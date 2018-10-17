23dc0b8bf62fca5f23f1b6236417ec238ac3b5e3
Joshua Montag <joshuamontag@Admins-MacBook-Pro.local>
2018-10-16 21:59:34 -0400

    platform crap

--- platforms/ios/build/PhoneTest.build/Debug-iphonesimulator/PhoneTest.build/Objects-normal/i386/NativeScriptEmbedder.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/joshuamontag/Documents/School/NMI-Capstone/beta/PhoneTest/platforms/ios/PhoneTest/PhoneTest-Prefix.pch \
-  /Users/joshuamontag/Documents/School/NMI-Capstone/beta/PhoneTest/node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.m \
-  /Users/joshuamontag/Documents/School/NMI-Capstone/beta/PhoneTest/platforms/ios/../../node_modules/tns-core-modules/platforms/ios/src/NativeScriptEmbedder.h
